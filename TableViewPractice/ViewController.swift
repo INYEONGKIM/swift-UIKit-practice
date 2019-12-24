ba8a07970984ec07ae42176b270c0e26a6314eee
inyeongkim <hacerrumbo@gmail.com>
2019-12-24 22:53:06 +0900

    Initial Commit

---
+++ TableViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by INYEONGKIM on 2019/12/24.
+//  Copyright © 2019 INYEONGKIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
